e2baf70b28fed44792f1aec02aeec05e3361c7d9
Frank-197 <frankomosh197@gmail.com>
2022-06-18 11:04:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+// Write your algorithm here
+   var lowerCase=word.toLowerCase()
+   var palindrome=lowerCase.split("").reverse().join("")
+   if (word==palindrome){
+    return true
+   }else{
+    return false
+   }
 }
-
+isPalindrome("eye")
 /* 
   Add your pseudocode here
+  The function palindrome takes in one argument which is a string
+  The function returns true if the function is a palindrome
+  The function returns true if the word is not a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  Created a function palindrome that took in an argument word.
+  Declared a variable lowerCase which converts the word parameter to lower case
+  Declared a variable palindrome which takes in the word and uses built in JavaScript functions to determine whether the word is a palindrome
+  Added an if function to determine if word passed satisfies conditions defined by palindrome
+  Called is palindrome and passed in an argument , eye to check whether it's a palindrome or not
 */
 
 // You can run `node index.js` to view these console logs
